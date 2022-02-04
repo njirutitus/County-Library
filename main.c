7e977d58d8b1724abd6a5f14431dc3f7e24307b6
Titus Njiru <njirutitus@gmail.com>
2022-02-04 13:31:41 +0300

    add menu sample

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu()
+{
+  printf("Select an action:\n");
+  printf("1. Add new patron\n");
+  printf("2. View Patrons\n");
+  printf("3. View Books\n");
+  printf("4. Add New Book\n");
+  printf("Your Action: \n");
+
+}
+
+int main()
+{
+    printf("COUNTY LIBRARY SYSTEM!\n");
+    printf("Welcome Mr. Titus!\n");
+    menu();
+    return 0;
+}
