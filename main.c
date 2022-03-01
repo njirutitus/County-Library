65b9e850c1e71214d2dc2c7868341c5a5a6e1c8c
Titus Njiru <njirutitus@gmail.com>
2022-03-01 16:41:45 +0300

    add execute_action

--- main.c
+++ main.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void menu()
+int menu()
 {
+  int action;
   printf("Select an action:\n");
   printf("1. Add new patron\n");
   printf("2. View Patrons\n");
   printf("3. View Books\n");
   printf("4. Add New Book\n");
-  printf("Your Action: \n");
-
+  printf("Your Action: ");
+  scanf("%d",&action);
+  if(action < 1 || action > 4) {
+    printf("Invalid action. Try again\n");
+  }
+  return action;
 }
+
+void execute_action(int action) {
+    switch(action) {
+    case 1:
+        printf("adding a new Patron\n");
+        break;
+    case 2:
+        printf("Here is a list of patrons\n");
+        break;
+    case 3:
+        printf("Here is a list of all books");
+        break;
+    case 4:
+        printf("adding a new Book\n");
+        break;
+    default:
+        printf("Invalid action.\n");
+    }
+}
+
+
 
 int main()
 {
     printf("COUNTY LIBRARY SYSTEM!\n");
     printf("Welcome Mr. Titus!\n");
-    menu();
+    execute_action(menu());
     return 0;
 }
