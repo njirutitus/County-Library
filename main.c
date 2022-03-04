a3e1fc103384e4b309435bbfe38a0f916934f12c
Titus Njiru <njirutitus@gmail.com>
2022-03-04 13:13:07 +0300

    add execute_action fucntion

--- main.c
+++ main.c
@@ -17,13 +17,34 @@ int menu() {
     printf("4. Add new Book\n");
     printf("Your action: ");
     scanf("%d",&action);
+    if(action < 1 || action > 4) {
+        printf("Invalid Action. Try again\n");
+    }
     return action;
+}
+
+void execute_action(int action) {
+    switch(action) {
+    case 1:
+        printf("adding a new patron\n");
+        break;
+    case 2:
+        printf("list of all patrons\n");
+        break;
+    case 3:
+        printf("list of all books\n");
+        break;
+    case 4:
+        printf("adding a new book\n");
+        break;
+    default: printf("Invalid action.\n");
+    }
 }
 
 int main()
 {
     printf("COUNTY LIBRARY SYSTEM\n");
     printf("Welcome Mr. Titus\n");
-    printf("You selected action %d",menu());
+    execute_action(menu());
     return 0;
 }
