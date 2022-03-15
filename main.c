d39fef97eb95f8353f049a1959303acf4456c178
Titus Njiru <njirutitus@gmail.com>
2022-03-15 17:03:53 +0300

    add patron struct and add patron function

--- main.c
+++ main.c
@@ -7,6 +7,28 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct Patron {
+    char name[100];
+    char email[50];
+    char password[30];
+    int is_staff;
+};
+
+void add_patron() {
+    struct Patron patron;
+    printf("Enter name: ");
+    getchar();
+    gets(patron.name);
+    printf("Enter Email: ");
+    gets(patron.email);
+    printf("Enter initial password: ");
+    gets(patron.email);
+    printf("Enter 1 if staff 0 otherwise");
+    scanf("%d",&patron.is_staff);
+    printf("%s added\n",patron.name);
+}
 
 int menu() {
     int action;
@@ -26,7 +48,7 @@ int menu() {
 void execute_action(int action) {
     switch(action) {
     case 1:
-        printf("adding a new patron\n");
+        add_patron();
         break;
     case 2:
         printf("list of all patrons\n");
